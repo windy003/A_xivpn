a08d53fc4376eaaa9d33bf9a0eb166add4412171
subtype2398 <173652916+Exclude0122@users.noreply.github.com>
2024-12-04 00:12:09 -0500

    fix fdroid build

--- prebuild-fdroid.sh
+++ prebuild-fdroid.sh
@@ -3,6 +3,7 @@
 echo "Prebuild"
 
 if [ ! -f "$HOME/go/bin/go" ]; then
+    cd $HOME
     echo "Downloading golang"
     curl -L https://go.dev/dl/go1.23.3.linux-amd64.tar.gz > go.tar.gz
     tar -xf go.tar.gz
